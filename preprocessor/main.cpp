fff7d01934beed7479aabc72ce6f9cd04c2ebd97
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-01-24 22:41:34 +0200

    Modified macros to accept 1 more argument and pass it along. Also covered case 0. Now prints triangle of pairs instead of rectangle. Added copyright notice. (found year of original version from file timestamp) Tested it with vc++, gcc and clang.

--- preprocessor/main.cpp
+++ preprocessor/main.cpp
@@ -1,20 +1,30 @@
+/* Copyright (c) 2009,2012 Vaptistis Anogeianakis */
+
 #include <iostream>
 using std::cout;
 using std::cin;
 using std::endl;
+
+#include <cstdlib>
+using std::system;
 
 #include "hv preprocessor substitutions.h"
 
 #define escape(s) xescape(s)
 #define xescape(s) #s
 
+#define concat(A,B) xconcat(A,B)
+#define xconcat(A,B) A##B
+
+
 int main()
 {
 #define PAIR(i,j) "(" escape(i) "," escape(j) ")"
-#define H_SUBSTITUTION_STRING(i,j) PAIR(i,j) ", "
-#define V_SUBSTITUTION_STRING(n) cout << SUBSTITUTE_HORIZONTALLY(2)(n) PAIR(2,n) << endl;
-	SUBSTITUTE_VERTICALLY(10)
+#define H_SUBSTITUTION_STRING(i,j,arg) PAIR(i,j) ", "
+#define V_SUBSTITUTION_STRING(n,arg) cout << SUBSTITUTE_HORIZONTALLY(n)(n,arg) PAIR(n,n) << endl;
+	SUBSTITUTE_VERTICALLY(10)(/**/)
 
-	system("PAUSE");
+
+		system("PAUSE");
 	return 0;
 } // end function main
